397035ada7918840561bd54ff4a0f68668fc34e5
acm-rgb <andrecm.pessoal@gmail.com>
2026-06-26 15:36:38 -0300

    Add profit margin calculator

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 const gramsInput = document.getElementById("grams");
 const filamentPriceInput = document.getElementById("filamentPrice");
+const profitMarginInput = document.getElementById("profitMargin");
 const calculateBtn = document.getElementById("calculateBtn");
 const result = document.getElementById("result");
 
 calculateBtn.addEventListener("click", function () {
     let grams = Number(gramsInput.value);
     let filamentPrice = Number(filamentPriceInput.value);
+    let profitMargin = Number(profitMarginInput.value);
 
-    if (grams <=0 || filamentPrice <=0) {
+    if (grams <=0 || filamentPrice <=0 || profitMargin <=0) {
         result.textContent = "Please enter valid numbers."
         return;
     }
 
     let materialCost = grams / 1000 * filamentPrice;
-    
-    result.textContent = `Material cost: ${materialCost.toFixed(2)} kr`
+    let margin = materialCost * (1 + profitMargin/100);
+
+    result.innerHTML = `
+        <span>Material cost: <strong>${materialCost.toFixed(2)} kr</strong></span>
+        <span>Suggested price: <strong>${margin.toFixed(2)} kr</strong></span>
+    `;
 })
